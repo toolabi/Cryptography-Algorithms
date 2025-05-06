81bdc17867bebf3bcc3ddd3e92390c66326949e4
toolabi <prds.toolabi@gmail.com>
2025-05-06 19:57:57 +0200

    fmt

--- Diffie_Hellman/src/main.rs
+++ Diffie_Hellman/src/main.rs
@@ -1,29 +1,25 @@
-
-use rand::{rng, Rng};
+use rand::{Rng, rng};
 fn main() {
     let p: u128 = 29;
-    let g :u128= 5;
+    let g: u128 = 5;
 
     // alice secret
-    let x: u128 =4;
+    let x: u128 = 4;
     // bob secret
-    let y :u128 = 8;
+    let y: u128 = 8;
 
-     // Calculate public values 
+    // Calculate public values
     let X = g.pow(x as u32) % p;
     let Y = g.pow(y as u32) % p;
 
-
     // Calculate shared secrets
-    let alice_k = Y.pow(x as u32) %p;
-    let bob_k = X.pow(y as u32) %p;
-
+    let alice_k = Y.pow(x as u32) % p;
+    let bob_k = X.pow(y as u32) % p;
 
     println!("Alice's secret: {}", x);
     println!("Bob's secret: {}", y);
     println!("Alice's shared key: {}", alice_k);
     println!("Bob's shared key: {}", bob_k);
-    
-    assert_eq!(alice_k,bob_k);
-}
 
+    assert_eq!(alice_k, bob_k);
+}
